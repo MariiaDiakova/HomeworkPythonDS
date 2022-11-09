d54e1feba07293788c2bd5c3e1238f22d2db5123
Mariia Diakova <mariadyakova97@gmail.com>
2022-11-10 00:12:29 +0200

    Add seasons_year function

--- Lesson_8/main.py
+++ Lesson_8/main.py
@@ -1,2 +1,15 @@
-def main_function(a):
-    pass
+def main_function():
+
+    def seasons_year(a):
+
+        if a > 12 or a < 1:
+            return 'Такого місяця немає!'
+
+        if a == 12 or a == 1 or a == 2:
+            return 'Зима'
+        elif a == 3 or a == 4 or a == 5:
+            return 'Весна'
+        elif a == 6 or a == 7 or a == 8:
+            return 'Літо'
+        else:
+            return 'Осінь'
